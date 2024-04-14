39a87bafb5465b6bfcf319ca17bc289f6510160c
Ebrahim Mozaffari <ebrahim.mozafary@gmail.com>
2024-04-14 18:35:15 +0100

    first UI develop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,24 +5,6 @@
       <TebexIcon class="customMargin mt-9 mb-7"  />
       <p class="text-left customMargin fontSize-15 mb-9">Your order</p>
 
-<!--      <v-row v-for="i in 3" :key="i" class="">-->
-<!--        <v-col v-if="!mobile"  lg="2" class="pa-1"></v-col>-->
-<!--        <v-col lg="9" sm="12" md="12" class="d-flex ml-5 pa-1 pr-9">-->
-<!--            <v-col lg="1"   class="ma-0 pa-0 v-col-1" align="center"-->
-<!--                   justify="center" >-->
-<!--              <v-img src="/images/small_coins.png" height="50" width="50" />-->
-<!--            </v-col>-->
-<!--            <v-col lg="11" md="11" sm="11" class="pa-0">-->
-<!--              <div class="text-body-1">Medium Booster</div>-->
-<!--              <div class="d-flex justify-space-between">-->
-<!--                <span class="text-caption">Qnty:1</span>-->
-<!--                <span  class="text-caption">9.99 USD</span>-->
-<!--              </div>-->
-<!--            </v-col>-->
-
-<!--        </v-col>-->
-<!--        <v-col v-if="!mobile" lg="2"></v-col>-->
-<!--      </v-row>-->
 
   <basketProduct v-for="i in 5" :key="i" />
       <div class="v-col-12 v-col-lg-9  mx-auto pl-5  mt-10" >
@@ -72,7 +54,88 @@
 
 
     <v-col lg="6" md="6" sm="12" class="v-col-12">
-      <p>dsfdsfdf</p>
+      <div class="v-col-9 mx-auto mt-16">
+        <v-breadcrumbs :items="items" class="ml-0 pl-2">
+          <template v-slot:divider>
+            <v-icon icon="mdi-chevron-right"></v-icon>
+          </template>
+        </v-breadcrumbs>
+
+        <div class="v-col-12">
+          <label>Email*</label>
+          <v-text-field
+            density="compact"
+            label="Enter your email address"
+            variant="solo"
+            hide-details
+            single-line
+
+          ></v-text-field>
+        </div>
+        <div class="v-col-12">
+          <label>Card number*</label>
+          <v-text-field
+            density="compact"
+            label="1234 5678 9101 1121"
+            variant="solo"
+            hide-details
+            single-line
+
+          ></v-text-field>
+        </div>
+        <v-row class="v-col-12">
+          <v-col class="v-col-3 pr-1">
+            <label>Expiry Date*</label>
+            <v-text-field
+              density="compact"
+              label="MM/YY"
+              variant="solo"
+              hide-details
+              single-line
+
+            ></v-text-field>
+          </v-col>
+          <v-col class="v-col-3 pl-0 pr-1">
+            <label>CVC/CVV*</label>
+            <v-text-field
+              density="compact"
+              label="123"
+              variant="solo"
+              hide-details
+              single-line
+
+            ></v-text-field>
+          </v-col>
+          <v-col class="v-col-6 pl-0">
+            <label>Zip Code / Postal Code*</label>
+            <v-text-field
+              density="compact"
+              label="N12 3ET"
+              variant="solo"
+              hide-details
+              single-line
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <div class="v-col-12">
+          <label>Name on card*</label>
+          <v-text-field
+            density="compact"
+            label="Enter your name as shown"
+            variant="solo"
+            hide-details
+            single-line
+
+          ></v-text-field>
+        </div>
+        <div class="v-col-12">
+          <v-btn block flat color="#41C4C3">Pay by Card</v-btn>
+        </div>
+
+
+      </div>
+
+
 
     </v-col>
   </v-row>
@@ -90,6 +153,20 @@ const { mobile } = useDisplay()
 // const isMobile = computed(()=>{
 //   return
 // })
+
+const items = [
+  {
+    title: 'Make Payment',
+    disabled: false,
+    href: 'breadcrumbs_dashboard',
+  },
+  {
+    title: 'Order Confirmed',
+    disabled: true,
+    href: 'breadcrumbs_link_1',
+  },
+
+]
 
 </script>
 
